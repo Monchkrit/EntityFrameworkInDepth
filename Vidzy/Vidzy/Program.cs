60e580c3631254a4f730911b8a77c144d81060fd
Donny Buckman <blitzkritter@gmail.com>
2019-11-18 17:33:40 +0000

    Initial Commit

---
+++ Vidzy/Vidzy/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vidzy
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var vidzy = new VidzyEntities();
+      vidzy.AddVideo("Field of Dreams", new DateTime(1999, 1, 1), "Family", 3);
+      vidzy.AddVideo("Mask", new DateTime(2001, 1, 15), "Comedy", 2);
+      vidzy.AddVideo("Escape From Alcatraz", new DateTime(1999, 12, 22), "Action", 2);
+
+    }
+  }
+}
